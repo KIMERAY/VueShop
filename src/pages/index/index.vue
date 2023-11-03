881d3043ec069df79a0c2a761f6bd57a9743f178
kimeray <kimerayforw@outlook.com>
2023-11-04 04:50:05 +0800

    feat: 轮播图类型定义及渲染

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,12 +1,28 @@
 <script setup lang="ts">
+import { getHomeBannerAPI } from '@/services/home'
+import type { BannerItem } from '@/types/home'
+import { onLoad } from '@dcloudio/uni-app'
+import { ref } from 'vue'
 import CustomNavbar from './components/CustomNavbar.vue'
+
+// 获取轮播图数据
+const bannerList = ref<BannerItem[]>([])
+const getHomeBannerData = async () => {
+  const res = await getHomeBannerAPI()
+  bannerList.value = res.result
+}
+
+// 页面加载
+onLoad(() => {
+  getHomeBannerData()
+})
 </script>
 
 <template>
   <!-- 自定义导航栏 -->
   <CustomNavbar />
   <!-- 自定义轮播图 -->
-  <XtxSwiper />
+  <XtxSwiper :list="bannerList" />
   <view class="index">index</view>
 </template>
 
--- src/utils/http.ts
+++ src/utils/http.ts
@@ -30,7 +30,6 @@ const httpInterceptor = {
     if (token) {
       options.header.Authorization = token
     }
-    console.log(options)
   },
 }
 uni.addInterceptor('request', httpInterceptor)
