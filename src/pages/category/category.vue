0b1006bd0d9d1dbd328313bf5304e8bc1be11775
kimeray <kimerayforw@outlook.com>
2023-11-10 02:16:09 +0800

    feat: 商品分类二级分类渲染和商品渲染

--- src/pages/category/category.vue
+++ src/pages/category/category.vue
@@ -5,6 +5,7 @@ import type { BannerItem } from '@/types/home'
 import type { CategoryTopItem } from '@/types/category'
 import { onLoad } from '@dcloudio/uni-app'
 import { ref } from 'vue'
+import { computed } from 'vue'
 
 // 获取轮播图数据
 const bannerList = ref<BannerItem[]>([])
@@ -15,6 +16,7 @@ const getBannerData = async () => {
 
 // 获取分类列表数据
 const categoryList = ref<CategoryTopItem[]>([])
+const activeIndex = ref(0)
 const getCategoryTopData = async () => {
   const res = await getCategoryTopAPI()
   categoryList.value = res.result
@@ -24,6 +26,11 @@ const getCategoryTopData = async () => {
 onLoad(() => {
   getBannerData()
   getCategoryTopData()
+})
+
+// 提取当前二级分类数据
+const subCategoryList = computed(() => {
+  return categoryList.value[activeIndex.value]?.children || []
 })
 </script>
 
@@ -43,7 +50,8 @@ onLoad(() => {
           v-for="(item, index) in categoryList"
           :key="item.id"
           class="item"
-          :class="{ active: index === 0 }"
+          :class="{ active: index === activeIndex }"
+          @tap="activeIndex = index"
         >
           <text class="name"> {{ item.name }} </text>
         </view>
@@ -53,27 +61,24 @@ onLoad(() => {
         <!-- 焦点图 -->
         <XtxSwiper class="banner" :list="bannerList" />
         <!-- 内容区域 -->
-        <view class="panel" v-for="item in 3" :key="item">
+        <view class="panel" v-for="item in subCategoryList" :key="item.id">
           <view class="title">
-            <text class="name">宠物用品</text>
+            <text class="name">{{ item.name }}</text>
             <navigator class="more" hover-class="none">全部</navigator>
           </view>
           <view class="section">
             <navigator
-              v-for="goods in 4"
-              :key="goods"
+              v-for="goods in item.goods"
+              :key="goods.id"
               class="goods"
               hover-class="none"
-              :url="`/pages/goods/goods?id=`"
+              :url="`/pages/goods/goods?id=${goods.id}`"
             >
-              <image
-                class="image"
-                src="https://yanxuan-item.nosdn.127.net/674ec7a88de58a026304983dd049ea69.jpg"
-              ></image>
-              <view class="name ellipsis">木天蓼逗猫棍</view>
+              <image class="image" :src="goods.picture"></image>
+              <view class="name ellipsis">{{ goods.name }}</view>
               <view class="price">
                 <text class="symbol">¥</text>
-                <text class="number">16.00</text>
+                <text class="number">{{ goods.price }}</text>
               </view>
             </navigator>
           </view>
