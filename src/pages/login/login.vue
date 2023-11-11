35b9949e3db9b065af925d69c721ffeb97d6958c
kimeray <kimerayforw@outlook.com>
2023-11-12 02:54:10 +0800

    feat: 登录模块-保存会员信息

--- src/pages/login/login.vue
+++ src/pages/login/login.vue
@@ -3,7 +3,9 @@
 <script setup lang="ts">
 import { postLoginWxMinAPI } from '@/services/login'
 import { postLoginWxMinSimpleAPI } from '@/services/login'
+import { useMemberStore } from '@/stores'
 import { onLoad } from '@dcloudio/uni-app'
+import type { LoginResult } from '@/types/member'
 
 // 获取 code 登录凭证
 let code = ''
@@ -21,13 +23,24 @@ const onGetphonenumber: UniHelper.ButtonOnGetphonenumber = async (ev) => {
     encryptedData,
     iv,
   })
-  console.log(res)
+  loginSuccess(res.result)
 }
 // 模拟手机号码快捷登录（开发练习）
 const onGetphonenumberSimple = async () => {
   const res = await postLoginWxMinSimpleAPI('18520107031')
-  console.log(res)
-  uni.showToast({ icon: 'none', title: '登录成功' })
+  loginSuccess(res.result)
+}
+
+const loginSuccess = (profile: LoginResult) => {
+  // 保存会员信息
+  const memberStore = useMemberStore()
+  memberStore.setProfile(profile)
+  // 成功提示
+  uni.showToast({ icon: 'success', title: '登录成功' })
+  setTimeout(() => {
+    // 页面跳转
+    uni.switchTab({ url: '/pages/my/my' })
+  }, 500)
 }
 </script>
 
