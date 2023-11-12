46fd87aceb1bcd73b0ede1bb00ce81d00c914370
kimeray <kimerayforw@outlook.com>
2023-11-13 01:17:47 +0800

    feat: 会员中心-个人信息页渲染

---
+++ src/services/profile.ts
@@ -0,0 +1,14 @@
+import type { ProfileDetail } from '@/types/member'
+import { http } from '@/utils/http'
+
+/**
+ * 获取个人信息
+ * @returns
+ */
+
+export const getMemberProfileAPI = () => {
+  return http<ProfileDetail>({
+    method: 'GET',
+    url: '/member/profile',
+  })
+}
