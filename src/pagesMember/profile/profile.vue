3fb54f90fd7431f5541a8356b6f54ae5a9480c76
kimeray <kimerayforw@outlook.com>
2023-11-14 02:20:48 +0800

    feat: 个人信息-职业修改

--- src/pagesMember/profile/profile.vue
+++ src/pagesMember/profile/profile.vue
@@ -74,7 +74,7 @@ const onFullLocationChange: UniHelper.RegionPickerOnChange = (ev) => {
 
 // 点击保存提交表单
 const onSubmit = async () => {
-  const { nickname, gender, birthday } = profile.value
+  const { nickname, gender, birthday, profession } = profile.value
   const res = await putMemberProfileAPI({
     nickname,
     gender,
@@ -82,6 +82,7 @@ const onSubmit = async () => {
     provinceCode: fullLocationCode[0],
     cityCode: fullLocationCode[1],
     countyCode: fullLocationCode[2],
+    profession,
   })
   // 更新Store昵称
   memberStore.profile!.nickname = res.result.nickname
@@ -159,7 +160,7 @@ const onSubmit = async () => {
         </view>
         <view class="form-item">
           <text class="label">职业</text>
-          <input class="input" type="text" placeholder="请填写职业" :value="profile?.profession" />
+          <input class="input" type="text" placeholder="请填写职业" v-model="profile!.profession" />
         </view>
       </view>
       <!-- 提交按钮 -->
