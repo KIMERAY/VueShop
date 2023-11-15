5b783d038f6ba8c019c779200c184ebeef21fdf6
kimeray <kimerayforw@outlook.com>
2023-11-16 03:39:36 +0800

    feat: 购物车模块-删除单品

--- src/pages/cart/cart.vue
+++ src/pages/cart/cart.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import { getMemberCartAPI } from '@/services/cart'
+import { deleteMemberCartAPI } from '@/services/cart'
 import { useMemberStore } from '@/stores'
 import { onShow } from '@dcloudio/uni-app'
 import { ref } from 'vue'
@@ -13,6 +14,22 @@ const cartList = ref<CartItem[]>([])
 const getMemberCartData = async () => {
   const res = await getMemberCartAPI()
   cartList.value = res.result
+}
+
+// 点击删除按钮
+const onDeleteCart = (skuId: string) => {
+  // 弹窗二次确认
+  uni.showModal({
+    content: '是否删除',
+    success: async (res) => {
+      if (res.confirm) {
+        // 后端删除单品
+        await deleteMemberCartAPI({ ids: [skuId] })
+        // 重新获取列表
+        getMemberCartData()
+      }
+    },
+  })
 }
 
 // 初始化调用: 页面显示触发
@@ -64,7 +81,7 @@ onShow(() => {
             <!-- 右侧删除按钮 -->
             <template #right>
               <view class="cart-swipe-right">
-                <button class="button delete-button">删除</button>
+                <button @tap="onDeleteCart(item.skuId)" class="button delete-button">删除</button>
               </view>
             </template>
           </uni-swipe-action-item>
--- src/services/cart.ts
+++ src/services/cart.ts
@@ -17,8 +17,20 @@ export const postMemberCartAPI = (data: { skuId: string; count: number }) => {
  * 获取购物车列表
  */
 export const getMemberCartAPI = () => {
-  return http<CartItem>({
+  return http<CartItem[]>({
     method: 'GET',
     url: '/member/cart',
+  })
+}
+
+/**
+ * 删除/清空购物车单品
+ * @param data 请求体参数 ids SKUID 集合
+ */
+export const deleteMemberCartAPI = (data: { ids: string[] }) => {
+  return http({
+    method: 'DELETE',
+    url: '/member/cart',
+    data,
   })
 }
